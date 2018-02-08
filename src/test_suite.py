7012f1ede20b07ac24faaeb9e8741ca7d908630c
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-02-07 21:32:36 -0500

    implemented code to display test images.

--- src/test_suite.py
+++ src/test_suite.py
@@ -1,11 +1,27 @@
 from data_utilities import load_practice_dataset
 import matplotlib.pyplot as plt
 
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_practice_dataset()
+
+
+def display_image(data_set, index):
+    plt.imshow(data_set[index])
+    plt.show()
+
+def show_some_images():
+    display_image(X_test_orig, 0)
+    display_image(X_train_orig, 1)
+
+    display_image(X_test_orig, 90)
+    display_image(X_train_orig, 723)
+
+
+    display_image(X_test_orig, 7)
+    display_image(X_train_orig, 1000)
+
 
 def main():
-    X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_practice_dataset()
-
-    plt.imshow(X_train_orig[0])
+    show_some_images()
 
     print("Testie")
 
