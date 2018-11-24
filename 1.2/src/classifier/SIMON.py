8b2e503e5de1450e30ead76e3af060f3b2a13b95
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-24 00:58:30 -0500

    Implemented a menu for SIMON

--- 1.2/src/classifier/SIMON.py
+++ 1.2/src/classifier/SIMON.py
@@ -18,6 +18,8 @@ class SIMON(object):
         response = 1
         while(response != 0):
             response = int(self.display_menu())
+            self.preform_action(menu_response = response)
+            print("Action Complete.\n")
 
     def perform_action(self, menu_response):
         if menu_response == 0:
