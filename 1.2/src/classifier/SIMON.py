879f5075e114f88eb10a05ee65b9e503388d5909
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-24 00:38:44 -0500

    Began working on SIMON.py

---
+++ 1.2/src/classifier/SIMON.py
@@ -0,0 +1,15 @@
+
+
+def menu():
+    print("Hello.  My name is SIMON.  I am a neural network designed to classify representations of american sign language.")
+    print("1 ) Load a previously trained model.")
+    print("2 ) Train a new model.")
+    print("3 ) predict an image.")
+    print("0 ) quit.")
+
+    return input("What would you like me to do : ")
+
+if __name__ == "__main__":
+    response = 1
+    while(response != 0):
+        response = menu()
