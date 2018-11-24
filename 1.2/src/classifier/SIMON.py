a62e6f2ba2bb37bef793547571292d6ebd6ef965
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-24 00:43:48 -0500

    Began working on SIMON.py

--- 1.2/src/classifier/SIMON.py
+++ 1.2/src/classifier/SIMON.py
@@ -1,15 +1,23 @@
 
+class SIMON(object):
+    def __init__(self):
+        pass
 
-def menu():
-    print("Hello.  My name is SIMON.  I am a neural network designed to classify representations of american sign language.")
-    print("1 ) Load a previously trained model.")
-    print("2 ) Train a new model.")
-    print("3 ) predict an image.")
-    print("0 ) quit.")
+    def display_menu(self):
+        print("Hello.  My name is SIMON.  I am a neural network designed to classify representations of american sign language.")
+        print("1 ) Load a previously trained model.")
+        print("2 ) Train a new model.")
+        print("3 ) predict an image.")
+        print("0 ) quit.")
 
-    return input("What would you like me to do : ")
+        return input("What would you like me to do : ")
+
+    def loop_menu(self):
+        response = 1
+        while(response != 0):
+            response = int(self.menu())
 
 if __name__ == "__main__":
-    response = 1
-    while(response != 0):
-        response = int(menu())
+    simon = SIMON()
+
+    simon.loop_menu()
