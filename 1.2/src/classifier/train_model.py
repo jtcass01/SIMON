2bf979a1e884c3f057ac1396fc324e53575a4533
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-24 02:31:04 -0500

    There seems to be an issue when I try to train a model inside of a SIMON object.  I am attempting to isolate the training using a commandline call and saving the model in between

---
+++ 1.2/src/classifier/train_model.py
@@ -0,0 +1,13 @@
+from ResNet50 import ResNet50
+import os
+
+if __name__ == "__main__":
+    print("Training new model under alias recent_model")
+    epochs = int(sys.argv[1])
+    batch_size = int(sys.argv[2])
+
+    model = ResNet50(input_shape = (64, 64, 3), classes = 6)
+    model.load_data_h5("../../../Practice_Data/")
+    model.train_model(epochs, batch_size)
+    model.evaluate_model()
+    model.save_model("recent_model")
